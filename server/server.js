0de4b2c16e375cb970de86103a8935ea0bee222d
Tamilarasan Lakshmanan <tamilarasan.lakshmanan@multco.us>
2018-02-06 21:31:16 -0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -39,6 +39,14 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) =>{
+    res.send({todos});
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
